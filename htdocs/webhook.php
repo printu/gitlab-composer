557cd0c2ec66a6b11c5468996303294438c84b6d
Paweł Krzaczkowski <pawel@freshmind.pl>
2019-07-09 07:41:56 +0200

    fix webhook timeout

--- htdocs/webhook.php
+++ htdocs/webhook.php
@@ -7,6 +7,10 @@ $confs = (new Config())->getConfs();
 $a = new AuthWebhook();
 $a->setConfig($confs);
 $a->auth();
+
+// respond to weebhook
+fastcgi_finish_request(); //this returns 200 to the user, and processing continues
+
 $Cr = new RegistryBuilder();
 $Cr->setConfig($confs);
 $Cr->update();
